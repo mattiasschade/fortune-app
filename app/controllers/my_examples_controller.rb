1f48f818548ce281b2189b7e82b52021fb850ab2
Mattias Schade <mattiasschade@gmail.com>
2023-03-27 14:26:16 -0500

    initialize fortune app

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -2,4 +2,13 @@ class MyExamplesController < ApplicationController
   def fortune
     render json: {message: "Here be your fortune:", fortune: ["Good!", "Bad :(", "just ok."].sample}
   end
+
+  def lottery
+    render json: {message: "Here be your lottery prediction:", numbers: (1..60).to_a.sample(6)}
+  end
 end
+
+
+
+  # def page_views
+  #   render json: {number: 0, message: "You've viewed this page #{number} times"}
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get "/fortune", controller: "my_examples", action: "fortune"
+
+  get "/lottery", controller: "my_examples", action: "lottery"
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
